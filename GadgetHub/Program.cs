8a20b1b6688761eb880aea47bdb3cfbcdd2562bb
Wale <onafesowale@gmail.com>
2023-08-26 11:37:11 +0100

    project file

--- GadgetHub/Program.cs
+++ GadgetHub/Program.cs
@@ -1,22 +1,29 @@
+using GadgetHub.Extensions;
+using GadgetHub.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext(builder.Configuration);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+await app.DatabaseInit();
+
+//app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseExceptionHandler("/Home/Error");
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
