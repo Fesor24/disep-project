0b0641347f2e3df403cc4129e8432abda8b82e16
Wale <onafesowale@gmail.com>
2023-08-26 11:33:49 +0100

    Entities

---
+++ GadgetHub/DataModels/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace GadgetHub.Models;
+
+public class Category : BaseEntity
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public ICollection<Product> Products { get; set; }
+}
---
+++ GadgetHub/DataModels/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace GadgetHub.Models;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public float Price { get; set; }
+
+    public string Image { get; set; }
+
+    public bool NewRelease { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public Category Category { get; set; }
+}
