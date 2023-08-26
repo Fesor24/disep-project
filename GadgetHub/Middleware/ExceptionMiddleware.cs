2bd2d3b28f28f7efc97c4196ec433f67044c4b36
Wale <onafesowale@gmail.com>
2023-08-26 11:35:19 +0100

    global exception handler

---
+++ GadgetHub/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using GadgetHub.ViewModels;
+using System.Text.Json;
+
+namespace GadgetHub.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IHostEnvironment _env;
+
+    public ExceptionMiddleware(RequestDelegate next, IHostEnvironment env)
+    {
+        _next = next;
+        _env = env;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch(Exception ex)
+        {
+            context.Response.StatusCode = 500;
+            context.Response.ContentType = "application/json";
+
+            var errors = _env.IsDevelopment() ? new ErrorViewModel { Message = ex.Message, Description = ex.StackTrace } :
+                new ErrorViewModel { Message = ex.Message, Description = string.Empty };
+
+            var jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var serializedError = JsonSerializer.Serialize(errors, jsonOptions);
+
+            await context.Response.WriteAsync(serializedError);
+        }
+    }
+}
