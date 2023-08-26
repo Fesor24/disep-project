693aaeed68704009093a23b7ed66d8c32772a1c2
Wale <onafesowale@gmail.com>
2023-08-26 20:15:12 +0100

    Shopping cart repository

---
+++ GadgetHub/Services/Implementation/ShoppingCartRepository.cs
@@ -0,0 +1,31 @@
+﻿using GadgetHub.Entities;
+using GadgetHub.Services.Abstractions;
+using Microsoft.IdentityModel.Tokens;
+using System.Text.Json;
+
+namespace GadgetHub.Services.Implementation;
+
+public class ShoppingCartRepository : IShoppingCartRepository
+{
+    public ShoppingCart GetCart(HttpContext context, string basketId = "basketId")
+    {
+        var shoppingCart = context.Session.GetString(basketId);
+
+        return shoppingCart.IsNullOrEmpty() ? null : JsonSerializer.Deserialize<ShoppingCart>(shoppingCart);
+    }
+
+    public ShoppingCart UpdateShoppingCart(HttpContext context, ShoppingCart cart, string basketId = "basketId")
+    {
+        context.Session.SetString(basketId, JsonSerializer.Serialize(cart));
+
+        return GetCart(context);
+    }
+
+    public bool DeleteShoppingCart(HttpContext context, string basketId = "basketId")
+    {
+        context.Session.Remove(basketId);
+
+        return true;
+    }
+
+}
