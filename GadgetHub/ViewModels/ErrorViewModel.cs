c35c914a231097259dfac59b4669858618230204
Wale <onafesowale@gmail.com>
2023-08-26 11:35:00 +0100

    View models

---
+++ GadgetHub/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GadgetHub.ViewModels;
+
+public class ErrorViewModel
+{
+    public string Message { get; set; }
+
+    public string Description { get; set; }
+}
---
+++ GadgetHub/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GadgetHub.ViewModels;
+
+public class ProductsViewModel
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public float Price { get; set; }
+
+    public string Image { get; set; }
+
+    public bool NewRelease { get; set; }
+}
