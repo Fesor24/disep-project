3cb8e258d56d40d1a1853c34ef6dc0a7462da35a
Wale <onafesowale@gmail.com>
2023-08-26 20:14:02 +0100

    Service extensions

---
+++ GadgetHub/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using GadgetHub.Services.Abstractions;
+using GadgetHub.Services.Implementation;
+
+namespace GadgetHub.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+
+        return services;
+    }
+}
