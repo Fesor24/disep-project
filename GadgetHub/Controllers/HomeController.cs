b94d19df9c87abe33e3fe4a450e9eb3bcd35158d
Wale <onafesowale@gmail.com>
2023-08-26 11:33:58 +0100

    Home controller

--- GadgetHub/Controllers/HomeController.cs
+++ GadgetHub/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
-﻿using System.Diagnostics;
+﻿using GadgetHub.Data;
+using GadgetHub.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using GadgetHub.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GadgetHub.Controllers;
 
@@ -8,24 +9,32 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
+    {
+        var newReleases = await _context.Products
+            .Where(x => x.NewRelease)
+            .Select(x => new ProductsViewModel
+            {
+                Name = x.Name,
+                Image = "https://localhost:7035" + x.Image.TrimStart('~'),
+                Price = x.Price,
+                Description = x.Description,
+                NewRelease = x.NewRelease
+            }).ToListAsync();
+
+        return View(newReleases);
+    }
+
+    public IActionResult About()
     {
         return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
