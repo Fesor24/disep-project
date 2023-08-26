4428991e3ba35e8d972b09fcd1c612eca7001dd5
Wale <onafesowale@gmail.com>
2023-08-26 11:34:10 +0100

    Application db context

---
+++ GadgetHub/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using GadgetHub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GadgetHub.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options): base(options)
+    {
+        
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach(var tracker in ChangeTracker.Entries<BaseEntity>())
+        {
+            tracker.Entity.DateUpdated = DateTimeOffset.UtcNow;
+
+            if(tracker.State == EntityState.Added)
+            {
+                tracker.Entity.DateCreated = DateTimeOffset.UtcNow;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+
+    public DbSet<Category> Categories => Set<Category>();
+}
