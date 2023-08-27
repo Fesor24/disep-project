d2d8df0b20cc07467266f19528d79c86423a5496
Wale <onafesowale@gmail.com>
2023-08-27 23:20:20 +0100

    CartItemSummary component

---
+++ GadgetHub/Components/CartItemSummary.cs
@@ -0,0 +1,46 @@
+﻿using GadgetHub.DataAccess.Abstractions;
+using GadgetHub.Entities;
+using GadgetHub.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GadgetHub.Components;
+
+public class CartItemSummary : ViewComponent
+{
+    private readonly IShoppingCartRepository _shoppingCartRepository;
+
+    public CartItemSummary(IShoppingCartRepository shoppingCartRepository)
+    {
+        _shoppingCartRepository = shoppingCartRepository;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var cart = _shoppingCartRepository.GetCart(HttpContext);
+
+        ShoppingCartViewModel scvm = new();
+
+        if (cart == null)
+        {
+            return View(scvm);
+        }
+
+        scvm = new ShoppingCartViewModel
+        {
+            Id = cart.Id,
+            Items = cart.Items.Select(x => new ShoppingCartItemViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Quantity = x.Quantity,
+                Image = x.Image,
+                Price = x.Quantity * x.Price,
+                CategoryId = x.CategoryId,
+            }).ToList()
+        };
+
+        return View(scvm);
+
+
+    }
+}
