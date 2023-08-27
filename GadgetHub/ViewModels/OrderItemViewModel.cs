c39c01bba9ae299736f99f5864ef9f8da94b6e46
Wale <onafesowale@gmail.com>
2023-08-27 23:23:27 +0100

    Order View Model

---
+++ GadgetHub/ViewModels/OrderItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GadgetHub.ViewModels;
+
+public class OrderItemViewModel
+{
+    public string ProductName { get; set; }
+
+    public string Image { get; set; }
+
+    public string Price { get; set; }
+
+    public int Quantity { get; set; }
+}
---
+++ GadgetHub/ViewModels/OrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GadgetHub.Entities.OrderAggregate;
+
+namespace GadgetHub.ViewModels;
+
+public class OrderViewModel
+{
+    public OrderStatus OrderStatus { get; set; }
+
+    public AddressViewModel Address { get; set; }
+
+    public string Subtotal { get; set; }
+
+    public string DeliveryCharges { get; set; }
+
+    public string Total { get; set; }
+
+    public List<OrderItemViewModel> OrderItems { get; set; } = new();
+}
