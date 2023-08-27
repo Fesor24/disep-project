7b081d4290d90f5eb491e7abcfc98ac54a22305a
Wale <onafesowale@gmail.com>
2023-08-27 23:24:19 +0100

    added validations

---
+++ GadgetHub/ViewModels/AddressViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GadgetHub.ViewModels;
+
+public class AddressViewModel
+{
+    [Required(ErrorMessage = "First name is required")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last name is required")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Street is required")]
+    public string Street { get; set; }
+
+    [Required(ErrorMessage = "City is required")]
+    public string City { get; set; }
+
+    [Required(ErrorMessage = "State is required")]
+    public string State { get; set; }
+}
