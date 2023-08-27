59217631622f86b556c39387fceaec998fd0a354
Wale <onafesowale@gmail.com>
2023-08-27 23:16:04 +0100

    Entity configurations

---
+++ GadgetHub/Data/Config/OrderConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using GadgetHub.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GadgetHub.Data.Config;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasMany(x => x.OrderItems).WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.OwnsOne(x => x.DeliveryAddress, io =>
+        {
+            io.WithOwner();
+        });
+
+        builder.Property(x => x.OrderStatus)
+            .HasConversion(c => c.ToString(),
+            p => (OrderStatus)Enum.Parse(typeof(OrderStatus), p));
+    }
+}
---
+++ GadgetHub/Data/Config/OrderItemConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using GadgetHub.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GadgetHub.Data.Config;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.OwnsOne(x => x.ItemOrdered, io =>
+        {
+            io.WithOwner();
+        });
+    }
+}
