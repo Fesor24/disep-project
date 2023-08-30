ebb7127d2a600179e0d3e4591a7a243fcfd1a0db
Wale <onafesowale@gmail.com>
2023-08-30 20:28:04 +0100

    payment models

---
+++ GadgetHub/Models/Payment.cs
@@ -0,0 +1,13 @@
+﻿namespace GadgetHub.Models;
+
+public class Payment
+{
+    public string Currency => "NGN";
+    public float Amount { get; set; }
+
+    public string Email { get; set; }
+
+    public int OrderId { get; set; }
+
+    public string CallbackUrl { get; set; }
+}
---
+++ GadgetHub/Models/PaymentResult.cs
@@ -0,0 +1,12 @@
+﻿namespace GadgetHub.Models;
+
+public class PaymentResult
+{
+    public string AuthorizationUrl { get; set; }
+    public string Reference { get; set; }
+
+    public string Message { get; set; }
+
+    public bool Successful { get; set; }
+
+}
