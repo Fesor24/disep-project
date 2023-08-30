1cf25953a6644388ef19d2735821bb0bf2c1a306
Wale <onafesowale@gmail.com>
2023-08-30 20:31:24 +0100

    Payment Transaction Repository

---
+++ GadgetHub/DataAccess/Abstractions/IPaymentTransactionRepository.cs
@@ -0,0 +1,8 @@
+﻿using GadgetHub.Entities;
+
+namespace GadgetHub.DataAccess.Abstractions;
+
+public interface IPaymentTransactionRepository : IGenericRepository<PaymentTransaction>
+{
+    Task<PaymentTransaction> GetByReferenceAsync(string reference);
+}
---
+++ GadgetHub/DataAccess/Implementation/PaymentTransactionRepository.cs
@@ -0,0 +1,21 @@
+﻿using GadgetHub.Data;
+using GadgetHub.DataAccess.Abstractions;
+using GadgetHub.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GadgetHub.DataAccess.Implementation;
+
+public class PaymentTransactionRepository : GenericRepository<PaymentTransaction>, IPaymentTransactionRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public PaymentTransactionRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<PaymentTransaction> GetByReferenceAsync(string reference)
+    {
+        return await _context.PaymentTransactions.FirstOrDefaultAsync(x => x.Reference == reference);
+    }
+}
