44bdec1bcfc822b0053b2300e08998eeb7458507
Wale <onafesowale@gmail.com>
2023-08-30 20:32:43 +0100

    Payment transaction entity

---
+++ GadgetHub/Entities/PaymentTransaction.cs
@@ -0,0 +1,14 @@
+﻿namespace GadgetHub.Entities;
+
+public class PaymentTransaction : BaseEntity
+{
+    public bool Verified { get; set; }
+
+    public int OrderId { get; set; }
+
+    public string CustomerEmail { get; set; }
+
+    public string Reference { get; set; }
+
+    public float Amount { get; set; }
+}
